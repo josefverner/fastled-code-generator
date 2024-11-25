1d055b20a65192d37344d4a4368ee80dd0687a3e
Josef Verner <josef.verner@gmail.com>
2024-11-25 18:01:04 +0100

    wip

--- src/app/services/array-converter.service.ts
+++ src/app/services/array-converter.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { StringType } from '../enums/string-type.enum';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArrayConverterService {
+  constructor() {}
 
-  constructor() { }
+  getArraySnakeLeftRight(arrayType?: StringType): string {
+    return `[
+      [0, 1, 2, 3, 4],
+      [9, 8, 7, 6, 5],
+      [10, 11, 12, 13, 14],
+      [19, 18, 17, 16, 15],
+      [20, 21, 22, 23, 24]
+    ]`;
+  }
 }
